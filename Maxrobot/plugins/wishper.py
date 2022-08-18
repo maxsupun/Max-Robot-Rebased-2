965eac260225705ea377419607a6ec85ad9f71f7
Supun Max <85874927+maxsupun@users.noreply.github.com>
2022-08-18 16:09:49 +0530

    Update wishper.py

--- Maxrobot/plugins/wishper.py
+++ Maxrobot/plugins/wishper.py
@@ -10,7 +10,7 @@ from pyrogram.types import (
 
 lengths = 200
 
-IMG = "https://telegra.ph/file/8bb5ad38249514dbf72e6.jpg"
+IMG = "https://telegra.ph/file/2fcbedac71233eeaa3ac9.jpg"
 
 @app.on_inline_query()
 async def wishper_ai(_, The: InlineQuery):
@@ -20,11 +20,11 @@ async def wishper_ai(_, The: InlineQuery):
             or (query.startswith('@') and len(split) == 1):
         title = f"🔐 Write a whisper message"
         content = ("**Send whisper messages through inline mode**\n\n"
-                   "Usage: `@TheMaxrobotbot [@username] text`")
-        description = "Usage: @TheMaxrobotbot [@username] text"
+                   "Usage: `@The_Max_Robot [@username] text`")
+        description = "Usage: @The_Max_Robot [@username] text"
         button = InlineKeyboardButton(
             "More-Help",
-            url="https://t.me/TheMaxrobotbot?start=learn"
+            url="https://t.me/The_Max_Robot?start=learn"
         )
 
     elif not query.startswith('@'):
@@ -72,11 +72,11 @@ __HELP__ = """
 the other way to use me is to write the inline query by your self
 the format should be in this arrangement
 
-@TheMaxrobotbot your whisper @username
+@The_Max_Robot your whisper @username
 
 now I'll split out the format in 3 parts and explain every part of it
 
-1- @TheMaxrobotbot
+1- @The_Max_Robot
 this is my username it should be at the beginning of the inline query so I'll know that you are using me and not another bot.
 
 2- whisper message
@@ -86,7 +86,7 @@ it is the whisper that will be sent to the target user, you need to remove your 
 you should replace this with target's username so the bot will know that the user with this username can see your whisper message.
 
 example:- 
-@TheMaxrobotbot hello this is a test @maxsupun
+@The_Max_Robot hello this is a test @Maxsupun1
 
 📎 the bot works in groups and the target user should be in the same group with you
 what you are waiting for?!
